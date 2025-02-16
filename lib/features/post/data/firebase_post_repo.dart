a96950d08cc1149e84d32e276c07377f51c6fefd
SahilKarwasra <sahilkarwasra11@gmail.com>
2025-02-17 01:23:05 +0530

    implemented main 4 important methods for posts

---
+++ lib/features/post/data/firebase_post_repo.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:social/features/post/domain/repository/post_repo.dart';
+
+import '../domain/entity/post.dart';
+
+class FirebasePostRepo implements PostRepo {
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // store the post in a collection named 'posts'
+  final CollectionReference postsCollection =
+      FirebaseFirestore.instance.collection('posts');
+
+  @override
+  Future<void> createPost(Post post) async {
+    try {
+      await postsCollection.doc(post.id).set(post.toJson);
+    } catch (e) {
+      throw Exception('Error while creating post: $e');
+    }
+  }
+
+  @override
+  Future<List<Post>> fetchAllPosts() async {
+    try {
+      // get all the post from the collection with new posts at the top
+      final postsSnapshot =
+          await postsCollection.orderBy('timestamp', descending: true).get();
+
+      // convert every firestore document into a list of Post object
+      final List<Post> allPosts = postsSnapshot.docs
+          .map((doc) => Post.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+
+      return allPosts;
+    } catch (e) {
+      throw Exception('Error while fetching posts: $e');
+    }
+  }
+
+  @override
+  Future<List<Post>> fetchPostByUserId(String userId) async {
+    try {
+      // fetch all the post from the collection with this uid
+      final postsSnapshot = await postsCollection
+          .where('uid', isEqualTo: userId)
+          .orderBy('timestamp', descending: true)
+          .get();
+
+      // convert every firestore document into a list of Post object
+      final userPosts = postsSnapshot.docs
+          .map((doc) => Post.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+
+      return userPosts;
+    } catch (e) {
+      throw Exception('Error while fetching posts: $e');
+    }
+  }
+
+  @override
+  Future<void> deletePost(String postId) async {
+    await postsCollection.doc(postId).delete();
+  }
+}

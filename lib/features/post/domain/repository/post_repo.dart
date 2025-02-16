3c5c8299f96533fbf41fb10cb14025e58ba8013f
SahilKarwasra <sahilkarwasra11@gmail.com>
2025-02-17 01:05:01 +0530

    Work on posts has started post entity and post_repo had been created

---
+++ lib/features/post/domain/repository/post_repo.dart
@@ -0,0 +1,8 @@
+import 'package:social/features/post/domain/entity/post.dart';
+
+abstract class PostRepo {
+  Future<List<Post>> fetchAllPosts();
+  Future<void> createPost(Post post);
+  Future<void> deletePost(String postId);
+  Future<List<Post>> fetchPostByUserId(String userId);
+}
